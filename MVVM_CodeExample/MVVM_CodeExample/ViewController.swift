8226857f5a3fba4dd7bd38117eeb909c0393fe69
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-25 18:19:48 +0200

    initial commit

---
+++ MVVM_CodeExample/MVVM_CodeExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_CodeExample
+//
+//  Created by Eslam Ali  on 25/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
