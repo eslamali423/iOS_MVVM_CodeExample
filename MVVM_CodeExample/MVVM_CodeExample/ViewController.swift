674b14956ab358d0a60fd6518843eb169f0f10c5
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-26 16:53:04 +0200

    create Tableview with customized cell

--- MVVM_CodeExample/MVVM_CodeExample/ViewController.swift
+++ MVVM_CodeExample/MVVM_CodeExample/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var models = [Person]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Radwa"
+        return cell
     }
 
 
+    
+}
+
+struct Person {
+    let name  :  String
+    let username  : String
+    let birtDate :  Date
+    let adress  :  String
 }
 
