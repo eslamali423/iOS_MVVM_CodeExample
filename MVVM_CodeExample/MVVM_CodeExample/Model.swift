66af4b987d5791fade05038090fd0a989dfe9bcf
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-26 20:58:03 +0200

    customize Models

---
+++ MVVM_CodeExample/MVVM_CodeExample/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  MVVM_CodeExample
+//
+//  Created by Eslam Ali  on 26/02/2022.
+//
+
+import Foundation
+class Model {
+    static let  shared =  Model ()
+    
+    
+    init () {}
+}
